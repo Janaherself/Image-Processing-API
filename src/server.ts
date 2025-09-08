057b2ccb7098966bf61f9cbd2ef60fe0dc57fad6
Jana Abusaa <jana.abusaa@gmail.com>
2025-09-08 06:41:29 +0300

    configure logging to a file with morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
+import { fileURLToPath } from 'url';
 import app from './index.js';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '..', 'logs', 'access.log'), {
+  flags: 'a',
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
+if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
